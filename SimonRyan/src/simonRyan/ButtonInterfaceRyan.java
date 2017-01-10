35a6ac46ef8684cc2f2c965c480a5e64ac79db1d
rsitu0946 <rsitu0946@bths.edu>
2017-01-10 13:03:40 -0500

    Update ButtonInterfaceRyan.java

--- SimonRyan/src/simonRyan/ButtonInterfaceRyan.java
+++ SimonRyan/src/simonRyan/ButtonInterfaceRyan.java
@@ -1,7 +1,28 @@
-package simonRyan;
+package simon;
 
+import java.awt.Color;
+
+import gui.Components.Action;
 import gui.Components.Clickable;
 
 public interface ButtonInterfaceRyan extends Clickable {
+
+	static Object getAButton() {
+		
+		return null;
+	}
+	
+	static void setAction(Action a){
+		
+	}
+
+	void setColor(Color blue);
+
+	void setX(int i);
+
+	void setY(int i);
+
+	void highlight();
+
 
 }
